1b7706d3805982f8642bdc8d3ea61ad68ec7647e
ionel.calin.stan@gmail.com <ionel.calin.stan@gmail.com>
2020-10-16 17:06:19 +0300

    am adaugat

---
+++ 1stApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1stApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
